86fe95185a046cce3eb90ffbed59a0b10fa4207c
fabiez <fabrice.badia@gmail.com>
2021-10-19 07:56:29 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Person fabz = new Person(1,
+                "Fabrice",
+                "Badia",
+                "fab@bad.com");
+
+        System.out.println(Person.);
+    }
+}
